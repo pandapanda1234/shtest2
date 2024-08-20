eabe2e26b1db471aa079b1fb06deab5360a05a10
Tomato1108 <y-komo@soft-hyperion.com>
2024-08-20 14:04:58 +0900

    メニューボタン変更

--- reacttest/src/LinkButtons.js
+++ reacttest/src/LinkButtons.js
@@ -1,9 +1,9 @@
 export default function LinkButtons() {
   return (
     <footer className="content">
-      <a href="/" className="link-button">Top</a>
-      <a href="/name-age" className="link-button">Name Age List</a>
-      <a href="/name-age/create" className="link-button">Create Person</a>
+      <a href="/" className="link-button">トップ</a>
+      <a href="/name-age" className="link-button">人物一覧</a>
+      <a href="/name-age/create" className="link-button">人物作成</a>
     </footer>
   );
 }
