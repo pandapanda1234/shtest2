4a54a1b5dd8d0986fdf03f435452c1c5fd865f87
r-suzuki <ri-suzuki@soft-hyperion.com>
2024-08-27 15:08:59 +0900

    JoinListRepository.java修正

--- React_DB/src/main/java/com/example/SpringBootTodo/repository/JoinListRepository.java
+++ React_DB/src/main/java/com/example/SpringBootTodo/repository/JoinListRepository.java
@@ -12,11 +12,10 @@ import com.example.SpringBootTodo.model.JoinList;
 public interface JoinListRepository extends JpaRepository<JoinList,Integer>{
 	
 	
-	//@QueryでSQL文を直接
+	//@Query内でcreatetb_db.name_age_list2とcreatetb_db.name_hobby_listをnameを基準に結合するSELECT文を書く
+	//←nativeQuery = true を指定することで、JPQL ではなくSQLによる問い合わせができる
 	@Query(value="SELECT createtb_db.name_age_list2.id,createtb_db.name_age_list2.name,createtb_db.name_age_list2.age,"
 			+ "createtb_db.name_hobby_list.hobby,createtb_db.name_hobby_list.skill FROM createtb_db.name_age_list2 "
 			+ "INNER JOIN createtb_db.name_hobby_list ON createtb_db.name_age_list2.name = createtb_db.name_hobby_list.name",nativeQuery=true)
+	//JoinList型のListを返り値とするメソッドJoinMethod()を作る←JoinMethod()を使用することで上記のクエリが実行される
 	public List<JoinList> JoinMethod();
-	
-
-}
