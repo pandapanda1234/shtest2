94799c62a35c39796d29d3bb2965a8a239d38bd0
r-suzuki <ri-suzuki@soft-hyperion.com>
2024-08-27 12:22:41 +0900

    ri-suzukicommit

--- reacttest/src/LinkButtons.js
+++
@@ -1,9 +0,0 @@
-export default function LinkButtons() {
-  return (
-    <footer className="content">
-      <a href="/" className="link-button">トップ</a>
-      <a href="/name-age" className="link-button">人物一覧</a>
-      <a href="/name-age/create" className="link-button">人物作成</a>
-    </footer>
-  );
-}
