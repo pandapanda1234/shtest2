7c3ad3a68b53b3eb650115c3a34b537e6dbe60fb
r-suzuki <ri-suzuki@soft-hyperion.com>
2024-08-29 09:15:28 +0900

    React変更

--- React/test/src/DBJoinInsert.js
+++ React/test/src/DBJoinInsert.js
@@ -1,56 +1,103 @@
+
+//reactからuseStateをインポートし、使えるようにする
 import {useState} from 'react';
 
-
+//引数Updateを持つDBJoinInsert関数を宣言する
 function DBJoinInsert({Update}){
 
+    //変数name、変数を更新する関数setNameを宣言し、変数の初期値に''(空文字)を設定する
     const [name,setName] = useState('');
+
+    //変数age、変数を更新する関数setAgeを宣言し、変数の初期値に''(空文字)を設定する
     const [age,setAge] = useState('');
+
+    //変数hobby、変数を更新する関数setHobbyを宣言し、変数の初期値に''(空文字)を設定する
     const [hobby,setHobby] = useState('');
+
+    //変数skill、変数を更新する関数setSkillを宣言し、変数の初期値に''(空文字)を設定する
     const [skill,setSkill] = useState('');
 
+    //変数message、変数を更新する関数setMessageを宣言し、変数の初期値に''(空文字)を設定する  
     const [message,setMessage] = useState('');
 
+    //引数eventを持つnameChange関数を宣言する
     const nameChange=(event)=>{
+
+        //引数event.target.valueからsetName関数を実行する
         setName(event.target.value);
     }
 
+    //引数eventを持つageChange関数を宣言する
     const ageChange=(event)=>{
+
+        //引数event.target.valueからsetAge関数を実行する
         setAge(event.target.value);
     }
 
+    //引数eventを持つhobbyChange関数を宣言する
     const hobbyChange=(event)=>{
+
+        //引数event.target.valueからsetHobby関数を実行する
         setHobby(event.target.value);
     }
 
+    //引数eventを持つskillChange関数を宣言する
     const skillChange=(event)=>{
+
+        //引数event.target.valueからsetSkill関数を実行する
         setSkill(event.target.value);
     }
 
     
 
-    
+    //引数eventを持つJoinInsertList関数を宣言する
     const JoinInsertList = (event)=>{
-        event.preventDefault();// デフォルトのフォーム送信を防ぐ
 
+        // デフォルトのフォーム送信を防ぐ←fetchでの操作をしたいから
+        event.preventDefault();
+
+        //変数AddJoinListに、{name:name,age:age,hobby:hobby,skill:skill}を代入する
         const AddJoinList = {name:name,age:age,hobby:hobby,skill:skill}
+
+        //変数urlに`http://localhost:8080/DBJoin/JoinListInsert`を代入する
         const url = `http://localhost:8080/DBJoin/JoinListInsert`
 
-
+        //変数url宛にfetchを行う
         fetch(url,{
+
+            //リクエストの種類をPOSTにする
             method:'POST',
+
+            //json形式のデータのヘッダー←json形式でデータを送ると宣言する
             headers:{'content-Type':'application/json',},
+
+            //引数AddJoinListをJSONのstringifyメソッドでJSON文字列に変換する←リクエストボディに設定する
             body:JSON.stringify(AddJoinList)
         })
+
+        //レスポンスを変数resに格納後、resをtext形式に変換する
         .then((res) => res.text())
+        //txt形式変換後のレスポンスを変数textに格納後、アロー関数を実行する
         .then((text) => {
+
+            //もしtextが"Error"なら実行する
             if(text === "Error"){
+
+                //引数textからsetMessage関数を実行する
                 setMessage(text);
+
+            //上記でないなら実行する
             }else{
+
+                //引数''(空文字)からsetMessage関数を実行する
                 setMessage('');
             }
         })
+
+        //エラーが発生したら引数"error"からalert関数を実行する
         .catch(() => alert("error"))
 
+        //Update関数を実行する←DBJoinコンポーネントからPropsされた関数
         Update();
 
     }
@@ -62,8 +109,14 @@ function DBJoinInsert({Update}){
         <>
         <div className='DBJoinInsert'>
             <div className='DBJoinInsert2'>
+
+            {/* Messageが空ではないなら変数Message内の値を表示する */}
             { message && <p style={{color: 'red'}}>{message}</p>}
+
+            {/* ボタンが押された時にonSubmit内の関数が実行される */}
             <form onSubmit={JoinInsertList}>
+
+                {/* input内の値が変更されるときにonChangeの関数を実行する */}
                 Name:<input name="name" value={name} onChange={nameChange}/><br/>
                 Age:<input name="age" value={age} onChange={ageChange}/><br/>
                 Hobby:<input name="hobby" value={hobby} onChange={hobbyChange}/><br/>
@@ -77,4 +130,5 @@ function DBJoinInsert({Update}){
 
 }
 
+//DBJoinInsert関数を主要コンポーネント(DBJoinInsert.jsの代表コンポーネント)としてエクスポートする
 export default DBJoinInsert;
--- React/test/src/DBJoinList.js
+++ React/test/src/DBJoinList.js
@@ -1,73 +1,94 @@
+
+//reactからuseState,useEffectをインポートし、使えるようにする
 import {useState,useEffect} from 'react'
-import { useNavigate } from 'react-router-dom';
 
-
+//引数にTFChangeを持つDBJoinList関数を宣言する
 function DBJointList({TFChange}){
 
+    //変数JoinListData,変数を更新する関数setJoinListDataを宣言し、変数の初期値に空の配列を設定する
     const [JoinListData,setJointListData] = useState([]);
+
+    //変数error、変数を更新する関数setErrorを宣言し、変数の初期値に''(空文字)を設定する
     const [error,setError] = useState('');
 
+    //[TFChange]のTFChangeが更新されるたびにuseEffect内の関数を実行する
     useEffect(()=>{
-        const JoinList= async()=>{
+
+        //JoinList関数を宣言する
+        const JoinList= ()=>{
+
+            //変数urlに`http://localhost:8080/DBJoin/JoinList`を代入する
             const url = `http://localhost:8080/DBJoin/JoinList`;
 
-            await fetch(url)
+            //変数url宛にfetchを行う
+            fetch(url)
+                //レスポンスを変数resに格納後、resをjson形式に変換する
                 .then((res) => res.json())
+                //json形式変換後のレスポンスを変数jsonに格納後、setJoinListData関数を実行する
                 .then((json) => setJointListData(json))
-                .catch(() => alert("error"))
-                .catch(() => setError)
+                //エラーが発生した時に実行される
+                .catch((err) => {
+                    //引数"error"からalert関数を実行する
+                    alert("error")
+                    //引数errからsetError関数を実行する
+                    setError(err)
+                })
+                
                  
         }
+
+        //JoinList関数を実行する
         JoinList();
     },[TFChange])
 
 
-    const navigate= useNavigate();
-    const Updatepage = (name) =>{
-        navigate('/DBJoinUpdate',{state:{name}});
-    }
-
-    
-   
-    
-
-    const JoinListMap = JoinListData.map((list,number)=>{
+    //変数JoinListMapに、JoinListData(配列)の要素を変数listとし、引数listから実行したアロー関数の結果を代入する
+    const JoinListMap = JoinListData.map((list)=>{
         return(
+            //各要素が一意にであることを示すため、keyに各要素のidを設定する
             <tr key={list.id}>
+                
+                {/* listのid,name,age,hobby,skillを表示する */}
                 <th>{list.id}</th>
                 <th>{list.name}</th>
                 <th>{list.age}</th>
                 <th>{list.hobby}</th>
                 <th>{list.skill}</th>
-                <th><button onClick={() =>Updatepage(list.name)}>更新</button></th>
             </tr>
         )
     })
 
 
-
-
-
     return(
         <>
         <div className='DBJoinList'>
+
+            {/* JoinListMapを表示するためのテーブル */}
             <table>
+                {/* 複数の<tr>をグループ化する */}
                 <tbody>
                     <tr>
+                        
+                        {/* カラム名の表示 */}
                         <td>ID</td>
                         <td>NAME</td>
                         <td>AGE</td>
                         <td>HOBBY</td>
                         <td>SKILL</td>
-                        <td>Update</td>
+                        
                     </tr>
+
+                    {/* JpinListMapの表示 */}
                     {JoinListMap}
                 </tbody>
             </table>
+
+            {/* errorが空ではないなら変数error内の値を表示する */}
             {error && <p style={{color:'red'}}>{error}</p>}
         </div>
         </>
     )
 }
 
+//DBJoinList関数を主要コンポーネント(DBJoinList.jsの代表コンポーネント)としてエクスポートする
 export default  DBJointList;
